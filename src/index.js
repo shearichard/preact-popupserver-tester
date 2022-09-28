b5d87c68458a873c36317c831add1fd6a7171e22
Richard Shea <rshea@thecubagroup.com>
2022-09-28 17:20:43 +1300

    Add a Navbar and some introductory text

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import './style';
 
 export default function App() {
 	return (
-		<div>
-			<h1>Hello, World!</h1>
+		<div className="container-fluid">
+			<nav className="navbar navbar-light bg-light">
+			  <div className="container-fluid">
+				<span className="navbar-brand mb-0 h1">Preact Sandbox Tester</span>
+			  </div>
+			</nav>
+			<div className="pt-4">
+				<div className="row">
+					<div className="col">
+						<h1 className="text-start">About</h1>
+						<p className="text-start">
+						A simple project to demonstrate how Preact differs from React in a simple CRUD environment.	
+						</p>
+					</div>
+					<div className="col">
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
